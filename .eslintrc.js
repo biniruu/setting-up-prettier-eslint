17a00526f843be24d4643e0fee258d021efb3584
biniruu <biniruu@gmail.com>
2021-09-08 17:45:54 +0900

    refactor: update dev environment to fit nuxt with jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,41 +3,22 @@ module.exports = {
   extends: [
     'eslint-config-prettier',
     'eslint-config-sinon',
-    // 'plugin:chai-friendly/recommended',
-    // 'plugin:mocha/recommended',
     'eslint:recommended',
-    // 'plugin:@typescript-eslint/recommended', // 타입스크립트 추천 룰셋
-    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    // 'plugin:jest/recommended',
-    // 'plugin:jsx-a11y/recommended', // react.js 프로젝트 시 필요
-    // 'plugin:nuxt/recommended',
+    'plugin:jest/recommended',
+    'plugin:nuxt/recommended',
     'plugin:prettier/recommended', // eslint의 포매팅 기능을 prettier로 사용. 항상 마지막에 세팅 되어야? (eslint-plugin-prettier)
-    // 'plugin:react/recommended',
-    // 'plugin:vue/strongly-recommended',
-    // 'prettier/@typescript-eslint', // eslint의 typescript 포매팅 기능을 제거 (eslint-config-prettier)
-    // 'prettier/react',
-    // 'tsconfig.json', // project: 'tsconfig.json'을 활성화 하기 위해 꼭 필요
+    'plugin:vue/strongly-recommended',
   ],
-  // globals: {
-  //   $nuxt: true,
-  // },
-  // ignorePatterns: ['.eslintrc.js'], // The file does not match your project config: eslintrc.js 에러 해결을 위해 필요
-  // parser: '@typescript-eslint/parser',
+  globals: {
+    $nuxt: true,
+  },
   parser: 'babel-eslint',
   parserOptions: {
-    // ecmaFeatures: {
-    //   jsx: true, // 리액트의 JSX 파싱을 위해서
-    // },
     ecmaVersion: 6,
-    // project: 'tsconfig.json', // parser: @typescript-eslint/parsergst를 활성화 하기 위해 꼭 필요
     sourceType: 'module', // 모듈 시스템 사용 시
   },
-  plugins: [
-    // 'react-hooks',
-  ],
   root: true,
   rules: {
-    // '@typescript-eslint/no-var-requires': 'error', // typescript에서 var 변수 사용 시 에러 발생
     'array-bracket-spacing': ['warn', 'never'], // 대괄호 안에 간격 삽입. always로 설정할 경우 prettier와 충돌 발생
     camelcase: ['error', { properties: 'never' }],
     'comma-dangle': 'off',
@@ -58,22 +39,9 @@ module.exports = {
     'prefer-rest-params': 'error',
     'prettier/prettier': ['warn', { bracketSpacing: true, printWidth: 120, singleQuote: true, trailingComma: 'all' }],
     quotes: ['warn', 'single', { allowTemplateLiterals: true }],
-    // 'react/prop-types': [
-    //   'warn',
-    //   {
-    //     // ignore: [], // optional array of props name to ignore during validation
-    //     // customValidators: [], // optional array of validators used for propTypes validation
-    //     skipUndeclared: true, // optional boolean to only error on components that have a propTypes block declared
-    //   },
-    // ],
     'space-before-function-paren': 'off', // allow debugger during development
-    // 'vue/v-bind-style': ['warn', 'shorthand'],
-    // 'vue/require-v-for-key': 'warn',
-    // 'vue/no-unused-components': 'warn',
+    'vue/v-bind-style': ['warn', 'shorthand'],
+    'vue/require-v-for-key': 'warn',
+    'vue/no-unused-components': 'warn',
   },
-  // settings: {
-  //   react: {
-  //     version: 'detect', // eslint-plugin-react가 자동 리액트버전탐지
-  //   },
-  // },
 }
--- README.md
+++ README.md
@@ -1,14 +1,4 @@
-# eslint-prettier-stylelint boilerplate
-
-프로젝트에 바로 사용할 수 있는 eslint-prettier-stylelint 설정.
-
-다음 라이브러리 & 프레임워크를 사용하는 프로젝트에 바로 적용할 수 있다.
-
-- Jest
-- React.js
-- Typescript
-- Vanillascript
-- Vue.js
+# eslint-prettier-stylelint boilerplate for nuxt with jest
 
 ## vs code에 prettier & eslint 설정하기
 
@@ -26,6 +16,7 @@
    1. prettier // 코드의 스타일을 잡아주는 포맷팅 기능
 
    ```bash
+   $ npm init -y
    $ yarn add -D babel-eslint eslint eslint-config-prettier eslint-loader eslint-plugin-import eslint-plugin-prettier prettier
    ```
 
@@ -81,52 +72,18 @@
 
    1. .stylelintrc.js
 
-## Vue.js 프로젝트일 때 추가 설정
+## Nuxt.js 프로젝트일 때 추가 설정
 
 1. npm 설치
 
    1. eslint-plugin-vue
+   1. eslint-plugin-nuxt
 
    ```bash
-   $ yarn add -D eslint-plugin-vue
+   $ yarn add -D eslint-plugin-vue eslint-plugin-nuxt
    ```
 
-1. .eslintrc.js에서 주석 처리한 vue 관련 옵션 활성화
-
----
-
-## Typescript 프로젝트일 때 추가 설정
-
-1. npm 설치
-
-   1. typescript
-   2. @typescript-eslint/eslint-plugin // Typescript 관련 린팅 규칙을 설정하는 플러그인
-   3. @typescript-eslint/parser // Typescript 를 파싱하기 위해 사용
-
-   ```bash
-   $ yarn add -D typescript @typescript-eslint/eslint-plugin @typescript-eslint/parser
-   ```
-
-1. .eslintrc.js에서 주석 처리한 typescript 관련 옵션 활성화
-1. .prettierrc에 `"parser": "typescript"` 추가
-
----
-
-## React.js 프로젝트일 때 추가 설정
-
-1. npm 설치
-
-   1. eslint-plugin-react
-   2. eslint-plugin-react-hooks
-   3. eslint-plugin-jsx-a11y
-
-   ```bash
-   $ yarn add -D eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-jsx-a11y
-   ```
-
-1. .eslintrc.js에서 주석 처리한 react 관련 옵션 활성화
-
----
+1. .eslintrc.js에서 주석 처리한 vue, nuxt 관련 옵션 활성화
 
 ## jest를 사용할 때 추가 설정
 
@@ -134,9 +91,10 @@
 
    1. eslint-plugin-jest
    1. jest
+   1. vue-jest
 
    ```bash
-   $ yarn add -D eslint-plugin-jest
+   $ yarn add -D eslint-plugin-jest jest vue-jest
    ```
 
 1. package.json > scripts 에 test 명령어 추가 후 실행
@@ -153,34 +111,3 @@
 
    - [Rules](https://www.npmjs.com/package/eslint-plugin-jest#user-content-rules)
    - [Typescript Rules](https://www.npmjs.com/package/eslint-plugin-jest#user-content-typescript-rules)
-
----
-
-## mocha를 사용할 때 추가 설정
-
-1. npm 설치
-
-   1. chai
-   1. eslint-config-sinon
-   1. eslint-plugin-chai-friendly
-   1. eslint-plugin-mocha
-   1. mocha
-   1. sinon
-
-   ```bash
-   $ yarn add -D chai eslint-config-sinon eslint-plugin-chai-friendly eslint-plugin-mocha mocha sinon
-   ```
-
-1. package.json > scripts 에 test 명령어 추가 후 실행
-
-   ```json
-   {
-     "scripts": {
-       "test": "mocha"
-     }
-   }
-   ```
-
-1. .eslintrc.js에서 주석 처리한 mocha 관련 옵션 활성화
-
-   [Rules](https://github.com/lo1tuma/eslint-plugin-mocha/tree/f9540209dc153c26fb44fa5250af6034058db875/docs/rules#rules)
