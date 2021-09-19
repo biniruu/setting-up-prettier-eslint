5fe3b5761ebc8dca2038919c5b6808874fc7cda1
biniruu <biniruu@gmail.com>
2021-09-20 02:06:09 +0900

    chore: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,15 @@
 module.exports = {
   env: { browser: true, es6: true, node: true },
-  extends: [
-    'eslint:recommended',
-    'eslint-config-prettier',
-    'plugin:import/recommended',
-    'plugin:prettier/recommended', // eslint의 포매팅 기능을 prettier로 사용. 항상 마지막에 세팅 되어야? (eslint-plugin-prettier)
-  ],
-  parserOptions: {
-    parser: 'babel-eslint',
-    ecmaVersion: 6,
-    sourceType: 'module', // 모듈 시스템 사용 시
-  },
+  extends: ['eslint:recommended', 'eslint-config-prettier', 'plugin:import/recommended', 'plugin:prettier/recommended'],
+  parserOptions: { parser: 'babel-eslint', ecmaVersion: 6, sourceType: 'module' },
   plugins: ['import', 'prettier'],
   root: true,
   rules: {
-    'array-bracket-spacing': ['warn', 'never'], // 대괄호 안에 간격 삽입. always로 설정할 경우 prettier와 충돌 발생
+    'array-bracket-spacing': ['warn', 'never'],
     camelcase: ['error', { properties: 'never' }],
     'comma-dangle': 'off',
     'computed-property-spacing': ['error', 'never', { enforceForClassMembers: false }],
-    'generator-star-spacing': 'off', // allow async-await
+    'generator-star-spacing': 'off',
     'import/no-unresolved': ['error', { commonjs: true, amd: true }],
     'new-cap': 'error',
     'no-array-constructor': 'error',
@@ -30,11 +21,10 @@ module.exports = {
     'no-new-object': 'error',
     'no-unused-vars': 'warn',
     'no-var': 'error',
-    'object-curly-spacing': ['warn', 'always'], // 중괄호 안에 간격 삽입
+    'object-curly-spacing': ['warn', 'always'],
     'prefer-const': 'error',
     'prefer-rest-params': 'error',
-    'prettier/prettier': ['warn', { bracketSpacing: true, printWidth: 120, singleQuote: true, trailingComma: 'all' }],
     quotes: ['warn', 'single', { allowTemplateLiterals: true }],
-    'space-before-function-paren': 'off', // allow debugger during development
+    'space-before-function-paren': 'off',
   },
 }
